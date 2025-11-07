0f2d3f025a61624b546a1c6627edfd67d1452a81
Patricia Felber <patricia.felber@t-online.de>
2025-11-07 12:23:03 +0100

    Auto-generated commit

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,17 +4,9 @@ importScripts(
 importScripts(
   "https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging-compat.js"
 );
+import { environment } from './environtment';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBLb6gK4thvmfCdEvaOqaxNrlo-ruOZV9o",
-    authDomain: "petcheck-c6543.firebaseapp.com",
-    projectId: "petcheck-c6543",
-    storageBucket: "petcheck-c6543.firebasestorage.app",
-    messagingSenderId: "370355042235",
-    appId: "1:370355042235:web:4e6ed52bd4b5ac8e39f69e"
-}
-
-const app = firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(environment.firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
