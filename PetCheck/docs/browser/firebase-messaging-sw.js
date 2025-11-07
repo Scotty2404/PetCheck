87cf6829aeb4a9ad606b849fb3f95b9943a9a107
Patricia Felber <patricia.felber@t-online.de>
2025-11-07 12:48:28 +0100

    deployed

---
+++ PetCheck/docs/browser/firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts(
+  "https://www.gstatic.com/firebasejs/11.4.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging-compat.js"
+);
+import { environment } from './environtment';
+
+const app = firebase.initializeApp(environment.firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[Service Worker] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
